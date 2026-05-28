2fca7510c49df4b010e2a370021bad4294dbd6ee
Lucas Eduardo Demétrio <lucaseduardodemetrio08@gmail.com>
2026-05-28 09:54:51 -0300

    add fivecon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,6 +89,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      {
+    rel: "icon",
+    type: "image/png",
+    href: "/favicon.jpg",
+      },
     ],
   }),
   shellComponent: RootShell,
